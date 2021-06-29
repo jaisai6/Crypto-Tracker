dba194ddf92cb3ce4978e957d8ce0c69730ca8c2
JAISAIKRISHNAN <jaisaikrishnan6@gmail.com>
2021-06-30 02:53:58 +0530

    Implemented live updation of cryptocurrency prices in frontend

--- dist/background.js
+++ dist/background.js
@@ -18,71 +18,98 @@ var hmac = crypto.createHmac('sha256', key);
 // hash the prehash string and base64 encode the result
 const signature = hmac.update(prehashString).digest('base64');
 
-chrome.runtime.onInstalled.addListener(() => {
+const init = {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+        'DEV-SHRIMPY-API-KEY': 'fe2e52350789d707a21f652bc6f7fa373f3ff77770d68b795894abea67e9d653',
+        'DEV-SHRIMPY-API-NONCE': nonce,
+        'DEV-SHRIMPY-API-SIGNATURE': signature,
+    }
+}
 
-    const init = {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'DEV-SHRIMPY-API-KEY': 'fe2e52350789d707a21f652bc6f7fa373f3ff77770d68b795894abea67e9d653',
-            'DEV-SHRIMPY-API-NONCE': nonce,
-            'DEV-SHRIMPY-API-SIGNATURE': signature,
+// Get crypto data from Shrimpy-api.
+async function getTicker() {
+    try {
+
+        const res = await fetch(requestPath, init);
+
+        if (!res.ok) {
+            throw new Error('Something went wrong');
         }
+
+        const coins = await res.json();
+
+        // console.log(coins.length);
+
+        console.log('Bitcoin: ', coins[0].priceUsd);
+
+        // Creating coinDetails
+        let coinDetails = [];
+        let genericPath = '../node_modules/cryptocurrency-icons/svg/color/';
+        let defaultPath = '../logo/logo.svg';
+
+        coins.forEach((coin, index) => {
+            // console.log(`${index}: ${coin.name} USD ${coin.priceUsd}`);
+
+            let item = {};
+            item.coinName = coin.name;
+            item.coinSymbol = coin.symbol;
+            item.coinPrice = coin.priceUsd;
+
+            // For coinIcon
+            let symbol = coin.symbol.toLowerCase();
+            let requiredPath = genericPath + `${symbol}.svg`;
+
+            // if (!fs.existsSync(requiredPath)) {
+            //     requiredPath = defaultPath;
+            // }
+
+            item.coinIcon = requiredPath;
+
+            // console.log(item);
+
+            coinDetails.push(item);
+
+            // console.log(coinDetails.length);
+        });
+
+        // console.log(coinDetails.length);
+
+        chrome.storage.local.set({ coinDetails });
+
+
     }
 
-    async function getTicker() {
-        try {
-
-            const res = await fetch(requestPath, init);
-
-            if (!res.ok) {
-                throw new Error('Something went wrong');
-            }
-
-            const coins = await res.json();
-
-            console.log(coins.length);
-
-            // Creating coinDetails
-            let coinDetails = [];
-            let genericPath = './node_modules/cryptocurrency-icons/svg/color/';
-
-            coins.forEach((coin,index) => {
-                console.log(`${index}: ${coin.name} USD ${coin.priceUsd}`);
-
-                let item = {};
-                item.coinName = coin.name;
-                item.coinSymbol = coin.symbol;
-                item.price = coin.priceUsd;
-                
-                // For coinIcon
-                let symbol = coin.symbol.toLowerCase();
-                let requiredPath = genericPath + `${symbol}.svg`;
-
-                if (!fs.existsSync(requiredPath)) {
-                    requiredPath = genericPath + 'generic.svg';
-                }
-
-                item.coinIcon = requiredPath;
-
-                console.log(item);
-
-                coinDetails.push(item);
-
-                console.log(coinDetails.length);
-            });
-            
-            console.log(coinDetails.length);
-
-            chrome.storage.local.set({coinDetails});
-
-        }
-
-        catch (err) {
-            console.log(err);
-        }
+    catch (err) {
+        console.log(err);
     }
+}
 
-    getTicker();
+
+chrome.runtime.onInstalled.addListener(async () => {
+
+    await getTicker();
 
 });
+
+
+
+chrome.alarms.onAlarm.addListener(async function (alarm) {
+    console.log("Alarm received at: ", Date());
+
+    await getTicker();
+
+    console.log('The current set price and bitcoin price must be same. Hence prices updated');
+
+    chrome.runtime.sendMessage('updated-crypto-data', function (res) {
+        console.log(res);
+        if (res == undefined){
+            console.log('popup.js is dead');
+            chrome.alarms.clear('get-crypto-data', (wasCleared) => console.log('Alarm is cleared: ', wasCleared));
+        }
+    });
+
+
+});
+
--- popup/popup.js
+++ popup/popup.js
@@ -1,16 +1,179 @@
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 
+//*******************************************************************************************
 
-chrome.storage.local.get('coinDetails', ({coinDetails}) => {
+var dropdown,searchInputDropdown,dropdownOptions,images;
 
-    console.log(coinDetails.length);
+dropdown  = document.querySelector('.table-body');
+
+// Update the cryptocurrency prices
+function CreateTable()
+{
+  chrome.storage.local.get('coinDetails', ({coinDetails}) => {
+
+    // console.log('Coin Details length: ', coinDetails.length);
 
     coinDetails.forEach(coinDetail => {
-        console.log(coinDetail.coinName, coinDetail.coinSymbol, coinDetail.coinIcon);
+        // console.log(coinDetail.coinName, coinDetail.coinSymbol, coinDetail.coinIcon, coinDetail.coinPrice);
+
+        var row = document.createElement('TR');
+        row.className = 'table-item';
+
+        //LOGO
+        var logo = document.createElement('TD');
+        logo.className = 'item-logo';
+
+        var img = document.createElement('IMG');
+        img.src = coinDetail.coinIcon;
+        img.className = 'item-logo';
+        img.alt = 'img';
+        // img.onerror = "this.src = '../logo/logo.svg';";
+
+        logo.appendChild(img);
+
+        //NAME
+        var name = document.createElement('TD');
+        name.textContent = coinDetail.coinName;
+        name.className = 'item-name';
+
+        //SYMBOL
+        var symbol = document.createElement('TD');
+        symbol.textContent = coinDetail.coinSymbol;
+        symbol.className = 'item-symbol';
+
+        //PRICE
+        var price = document.createElement('TD');
+        price.textContent = '$' + (Math.round(coinDetail.coinPrice*100000))/100000;
+        price.className = 'item-price';
+        
+        //APPENDING ALL TO TD
+        row.appendChild(logo);
+        row.appendChild(name);
+        row.appendChild(symbol);
+        row.appendChild(price);
+
+        //APPENDING TD TO TD
+        dropdown.appendChild(row);
+
     });
+    
+    
+    searchInputDropdown = document.querySelector('.form-control');
+    dropdownOptions = document.querySelectorAll('.table-item');
+    images = document.querySelectorAll('img');
+    searchInputDropdown.addEventListener('input',() => {
+
+        filterData();
+    });
+      
+    dropdown.addEventListener('click',(e)=>{
+          // console.log(e);
+        //   if( e.path[0]!= null && e.path[0].className === 'table-item')
+        //       searchInputDropdown.value = e.path[0].textContent;
+        //   filterData();
+    }) ;
+
+    images.forEach(image=>{
+      image.addEventListener('error',()=>{
+          image.src = '../node_modules/cryptocurrency-icons/svg/color/generic.svg';
+      });
+    });
+    
 });
+};
+
+CreateTable();
+
+
+function UpdateTable()
+{
+  chrome.storage.local.get('coinDetails', ({coinDetails}) => {
+
+    // console.log('Coin Details length: ', coinDetails.length);
+    var children = dropdown.children;
+    coinDetails.forEach((coinDetail,index) => {
+        // console.log(coinDetail.coinName, coinDetail.coinSymbol, coinDetail.coinIcon, coinDetail.coinPrice);
+          var row = children[index];
+          // console.log('row: ',row);
+          var tdChildren = row.children;
+          // console.log('tdChildren: ',tdChildren);
+          // console.log('tdChildren[3]: ',tdChildren[3]);
+          tdChildren[3].textContent = '$' + (Math.round(coinDetail.coinPrice*100000))/100000;
+
+    });
+    
+    
+    searchInputDropdown = document.querySelector('.form-control');
+    dropdownOptions = document.querySelectorAll('.table-item');
+
+    searchInputDropdown.addEventListener('input',() => {
+
+        filterData();
+    });
+      
+    dropdown.addEventListener('click',(e)=>{
+          // console.log(e);
+        //   if( e.path[0]!= null && e.path[0].className === 'table-item')
+        //       searchInputDropdown.value = e.path[0].textContent;
+        //   filterData();
+    }) ;
+});
+};
+
+
+
+//*******************************************************************************************
+
+
+// Initiate an "Alarm" function to send "get-data" message to background.js and update the data displayed in frontend
+
+chrome.alarms.create('get-crypto-data', {delayInMinutes: 0.5, periodInMinutes: 1});
+
+
+// Listen to the response from background.js after the alarm
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    
+    if (message === 'updated-crypto-data') {
+        UpdateTable(); // Updating the frontend
+        sendResponse('I am still alive'); // Sending a response back to background.js to let it know that popup.js is still open.
+    }
+
+  });
+  
+
+//*******************************************************************************************
 
 // Modify the DOM to display the list of coins.
+
+// searchInputDropdown.addEventListener()
+
+function filterData() {
+  const filter = searchInputDropdown.value.toLowerCase();
+  // console.log('yes');
+//   console.log(el.firstChild.nextSibling);
+  showOptions();
+  const valueExist = !!filter.length;
+
+  if (valueExist) {
+    dropdownOptions.forEach((el) => {
+      // console.log(el.firstChild.nextSibling);
+      const elText = el.firstChild.nextSibling.textContent.trim().toLowerCase();
+      const isIncluded = elText.includes(filter);
+      if (!isIncluded) {
+        el.style.display = 'none';
+      }
+    });
+  }
+}
+
+const showOptions = () => {
+  dropdownOptions.forEach((el) => {
+    el.style.display = 'table-row';
+  })
+}
+
+
 
 
 
