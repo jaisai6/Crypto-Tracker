02d065dfe7e229e1ae098175a2e48f83bfd9e7a0
JAISAIKRISHNAN <jaisaikrishnan6@gmail.com>
2021-06-30 02:56:00 +0530

    Removed bootstrap import in popup.js

--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,3 @@
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
-
 //*******************************************************************************************
 
 var dropdown,searchInputDropdown,dropdownOptions,images;
